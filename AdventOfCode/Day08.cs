345ef78f3959904a7bd32d4d42c972aaad6b61f9
scalonne <calonne.sebastien+github@gmail.com>
2024-02-04 13:21:34 +0100

    day08

---
+++ AdventOfCode/Day08.cs
@@ -0,0 +1,64 @@
+﻿namespace AdventOfCode;
+
+public class Day08 : BaseDay
+{
+    Queue<char> Path;
+    Dictionary<string, (string left, string right)> NodeDic;
+
+    public Day08() {
+        var lines  = File.ReadAllLines(InputFilePath);
+
+        Path = new Queue<char>(lines[0]);
+        NodeDic = lines.Skip(2)
+                       .Select(o => o.Split(" = "))
+                       .Select(o => (key: o[0], lr: o[1].Substring(1, o[1].Length - 2)
+                                                        .Split(", ")))
+                       .ToDictionary(o => o.key, o => (left: o.lr[0], right: o.lr[1]));
+    }
+
+    public override ValueTask<string> Solve_1() => new(Part_1("AAA", node => node == "ZZZ").ToString());
+
+    public override ValueTask<string> Solve_2() => new(Part_2().ToString());
+
+    int Part_1(string node, Func<string, bool> stoppingFunc) {
+        var steps = 0;
+
+        while (true) {  
+            var path = new Queue<char>(Path);
+
+            while (path.TryDequeue(out var dir)) {
+                node = dir == 'L' ? NodeDic[node].left : NodeDic[node].right;
+
+                steps++;
+
+                if (stoppingFunc(node))
+                    return steps;
+            }
+        }
+    }
+
+    ulong Part_2() {
+        var steps = new Queue<ulong>(NodeDic.Keys.Where(o => o.Last() == 'A')
+                                                 .Select(o => (ulong)Part_1(o, node => node.Last() == 'Z')));
+        var lcm = steps.Dequeue();
+
+        while (steps.TryDequeue(out var step))
+            lcm = LeastCommonMultiple(lcm, step);
+
+        return lcm;
+    }
+
+    ulong GreatestCommonFactor(ulong a, ulong b) {
+        while (b != 0) {
+            var temp = b;
+
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
+    }
+
+    ulong LeastCommonMultiple(ulong a, ulong b)
+        => a / GreatestCommonFactor(a, b) * b;
+}
