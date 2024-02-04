22fa99398f304bd2a8b39b883cd0b051a95ed666
scalonne <calonne.sebastien+github@gmail.com>
2024-02-04 14:38:08 +0100

    CollectionUtils.csproj
    EnumerableExtensions - Sequence

---
+++ CollectionsUtils/EnumerableExtensions.cs
@@ -0,0 +1,11 @@
+﻿public static partial class EnumerableHelper {
+    public static IEnumerable<int> Sequence(int start, int count, int steps) {
+        if (count == 0)
+            yield break;
+
+        while (count-- > 0) {
+            yield return start;
+            start += steps;
+        }
+    }
+}
